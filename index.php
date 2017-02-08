d3058b033d36918953444205ae6b62ed9bed4f06
MOFH MOD <labxdz@gmail.com>
2017-02-08 18:58:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-its working!!
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="EN" lang="en">
+<head>
+<meta name="robots" content="noindex" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="Login/inscription" />
+<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0" />
+<title>Login</title>
+<link  type="image/png" href="/assets/img/logo/favicon.png" rel="icon" />
+<link  href="/assets/img/logo/touch-icon.png" rel="apple-touch-icon" />
+	
+		<link type="text/css" href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" /><link type="text/css" href="https://static-openask-com.s3.amazonaws.com/assets/css/main.b1cd613e.css?t=1486575526" rel="stylesheet" />
+  <link type="text/css" href="/assets/css/theme.css" rel="stylesheet" />	
+  </head>	
+<body>
+  it's working
+  </body>
+</html>
